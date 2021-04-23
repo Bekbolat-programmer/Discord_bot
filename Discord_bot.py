bce715cbd033e4a3e4f815b88264bc7b94233ac5
Daniil-Cir <svetlanka.vorobeva.1977@mail.ru>
2021-04-23 20:50:36 +0600

    Discord_done

--- Discord_bot.py
+++ Discord_bot.py
@@ -1,23 +1,32 @@
-import discord
 import random
 from discord.ext import commands
+from discord.ext.commands import Bot
 import discord.utils
 import requests
 import youtube_dl
-import os
 import sqlite3
 from translate import Translator
 import datetime
-import schedule
+import os
+
 
 
 dashes = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
 TOKEN = "ODMxOTEzMDA5ODY1MTYyNzUy.YHcJXQ.ij7L_yhL6Cc8pztzWMlTHK46PWk"
-
+ban_words = ["политика", "мачу-пикчу", "чихуа-хуа"]
+support = ["Поздравляем", "Молодец", "Ююююхууууу"]
 
 class Multi_Bot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        for i in ban_words:
+            if i in message.content.lower():
+                await message.delete()
+        if "ура" in message.content.lower():
+            await message.channel.send(random.choice(support))
 
     @commands.command(name='help1', aliases=["h"])
     async def help1(self, ctx, *arg):
@@ -177,11 +186,11 @@ class Multi_Bot(commands.Cog):
                     con.commit()
                     await ctx.send("Операция прошла успешно)")
                 else:
-                    await ctx.send("Введен несуществующий ранг(")
+                    await ctx.send("Введен несуществующий ранг")
             else:
                 await ctx.send("Ошибка при вводе команды(возможно вы передали недостаточно аргументов)")
         else:
-            await ctx.send("У вас нет прав на выполнение данной команды")
+            await ctx.send("У вас нет прав на выполнение данной команды 🤔")
         con.close()
 
     @commands.command(aliases=["en"])
@@ -193,38 +202,148 @@ class Multi_Bot(commands.Cog):
 
     @commands.command(aliases=["ru"])
     async def ru_en(self, ctx, *args):
-        txt = str(" ".join(args))
-        translator = Translator(from_lang="Russian", to_lang="English")
-        translation = translator.translate(txt)
-        await ctx.send(translation)
+        try:
+            txt = str(" ".join(args))
+            translator = Translator(from_lang="Russian", to_lang="English")
+            translation = translator.translate(txt)
+            await ctx.send(translation)
+        except:
+            await ctx.send("Ошибка выполнения команды.😔")
+            await ctx.send("Возможно вы перево дите не с русского языка")
 
     @commands.command(aliases=["t"])
     async def timer(self, ctx, *args):
-        time = ''.join(args).lower()
-        if ':' in time:
-            t = time.split(":")
-            hours = t[0]
-            minutes = t[1]
-            while True:
-                if int(datetime.datetime.now().strftime("%H")) == int(hours)\
-                        and int(datetime.datetime.now().strftime("%M")) == int(minutes):
-                    await ctx.send(f'⏰ Time X has come!')
-                    break
-
-        else:
-            hours = int(time.split('hours')[0])
-            minutes = int(time.split('hours')[1].split('minutes')[0])
-            flag = True
-            time_now = datetime.datetime.now()
-            await ctx.send(f'The timer should start in {hours} hours and {minutes} minutes.')
-            if flag:
+        try:
+            time = ''.join(args).lower()
+            if ':' in time:
+                t = time.split(":")
+                hours = t[0]
+                minutes = t[1]
                 while True:
-                    delta = datetime.datetime.now() - time_now
-                    if delta.seconds >= hours * 3600 + minutes * 60:
-                        hours, minutes, flag = 0, 0, False
-                        time = None
+                    if int(datetime.datetime.now().strftime("%H")) == int(hours)\
+                            and int(datetime.datetime.now().strftime("%M")) == int(minutes):
+                        await ctx.send(f'⏰ Time X has come!')
                         break
-                await ctx.send(f'⏰ Time X has come!')
+
+            else:
+                hours = int(time.split('hours')[0])
+                minutes = int(time.split('hours')[1].split('minutes')[0])
+                flag = True
+                time_now = datetime.datetime.now()
+                await ctx.send(f'The timer should start in {hours} hours and {minutes} minutes.')
+                if flag:
+                    while True:
+                        delta = datetime.datetime.now() - time_now
+                        if delta.seconds >= hours * 3600 + minutes * 60:
+                            hours, minutes, flag = 0, 0, False
+                            time = None
+                            break
+                    await ctx.send(f'⏰ Time X has come!')
+        except:
+            await ctx.send(f'Команды была введена неправильно 😞')
+            await ctx.send(f'Воспользуйтесь командой `-help_game`, чтобы сверить запрос')
+
+    @commands.command(pass_context=True)
+    async def kick(self, ctx, user: discord.Member, reason=None):
+        try:
+            con = sqlite3.connect("data/tabels/rangs.db")
+            cur = con.cursor()
+            result = cur.execute(
+                f"SELECT login FROM logins WHERE rang_id = (SELECT id FROM rangs WHERE rang LIKE 'admin')"
+            ).fetchall()
+            admins = []
+            for elem in result:
+                admins.append(elem[0])
+            if str(ctx.message.author) in admins:
+                await user.kick(reason=reason)
+                await ctx.send(f"Вы успешно кикнули {user} 😞")
+            else:
+                await ctx.send(f'У вас нет прав на выполнение данной команды 😕')
+        except:
+            await ctx.send(f'Неверно введен пользователь 🤔')
+
+    @commands.command()
+    async def ban(self, ctx, user: discord.Member, *reason):
+        try:
+            con = sqlite3.connect("data/tabels/rangs.db")
+            cur = con.cursor()
+            result = cur.execute(
+                f"SELECT login FROM logins WHERE rang_id = (SELECT id FROM rangs WHERE rang LIKE 'admin')"
+            ).fetchall()
+            admins = []
+            for elem in result:
+                admins.append(elem[0])
+            if str(ctx.message.author) in admins:
+                await user.ban(reason=" ".join(reason))
+                await ctx.send(f"{user} был __успешно__ забанен на сервере 😞")
+            else:
+                await ctx.send(f'У вас нет прав на выполнение данной команды 😕')
+        except:
+            await ctx.send(f'Неверно введен пользователь 🤔')
+
+    @commands.command()
+    async def unban(self, ctx, user: discord.Member):
+        try:
+            con = sqlite3.connect("data/tabels/rangs.db")
+            cur = con.cursor()
+            result = cur.execute(
+                f"SELECT login FROM logins WHERE rang_id = (SELECT id FROM rangs WHERE rang LIKE 'admin')"
+            ).fetchall()
+            admins = []
+            for elem in result:
+                admins.append(elem[0])
+            if str(ctx.message.author) in admins:
+                await user.unban()
+                await ctx.send(f"Разбан пользователя{user} был __успешно__ выполнен 👏")
+            else:
+                await ctx.send(f'У вас нет прав на выполнение данной команды 😕')
+        except:
+            await ctx.send(f'Неверно введен пользователь или данный пользователь не был забанен 😉')
+
+    @commands.command(aliases=["g"])
+    async def game(self, ctx, *args):
+        print(len(args), args)
+        if len(args) != 0:
+            genre = " ".join(args).lower()
+            con = sqlite3.connect("data/tabels/games.db")
+            cur = con.cursor()
+            result = cur.execute(f"SELECT id FROM genres WHERE genre LIKE '%{genre}%'").fetchall()
+            if len(result) != 0:
+                games = cur.execute(f"SELECT * FROM games WHERE genre_id = {result[0][0]}").fetchall()
+                game = random.randint(0, len(games))
+                for i in range(1, len(games[game])):
+                    if i == 2:
+                        pass
+                    else:
+                        await ctx.send(games[game][i])
+                cur.close()
+            else:
+                games = cur.execute(f"SELECT * FROM games").fetchall()
+                game = random.randint(0, len(games))
+                await ctx.send("Извините у нас нет игры в таком жанре 😞")
+                await ctx.send("❗__Но мы можем предложить:__❗")
+                for i in range(1, len(games[game])):
+                    if i == 1:
+                        await ctx.send(f'`{games[game][i]}`')
+                    elif i == 2:
+                        pass
+                    else:
+                        await ctx.send(games[game][i])
+                cur.close()
+        else:
+            con = sqlite3.connect("data/tabels/games.db")
+            cur = con.cursor()
+            games = cur.execute(f"SELECT * FROM games").fetchall()
+            game = random.randint(0, len(games))
+            await ctx.send("❗__Вот что мы можем предложить__❗(надеемся вы хорошо проведете время):")
+            for i in range(1, len(games[game])):
+                if i == 1:
+                    await ctx.send(f'`{games[game][i]}`')
+                elif i == 2:
+                    pass
+                else:
+                    await ctx.send(games[game][i])
+            cur.close()
 
 
 bot = commands.Bot(command_prefix='-')
